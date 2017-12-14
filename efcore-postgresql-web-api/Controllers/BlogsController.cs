7de425c0f743ccaa5b8411988948ab5f59e3d154
BjornChaudron <bjornchaudron@gmail.com>
2017-12-14 15:19:50 +0100

    WTF

---
+++ efcore-postgresql-web-api/Controllers/BlogsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using efcore_postgresql_web_api.Entities;
+using efcore_postgresql_web_api.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace efcore_postgresql_web_api.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class BlogsController : Controller
+    {
+        private IRepository<Blog> Repository { get; }
+
+        public BlogsController(IRepository<Blog> repository)
+        {
+            Repository = repository;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]Blog blog)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
